228214ac844d137243a773f92b9354277e27fe71
SharDezZz1 <andriypidkova27@gmail.com>
2023-05-21 15:10:40 +0300

    start point

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+//impory bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layouts from '@/layouts';
+//redux
+import { Provider } from 'react-redux';
+import { store } from '@/store';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+    <Layouts>
+      <Component {...pageProps} />
+    </Layouts>
+    </Provider>
+  )
 }

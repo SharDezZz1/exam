1f7f9a50a2bbad67321c3296b9bfb973d3486949
SharDezZz1 <andriypidkova27@gmail.com>
2023-05-27 14:41:19 +0300

    hero and header

--- src/components/CheckAvailability/index.tsx
+++ src/components/CheckAvailability/index.tsx
@@ -1,17 +1,37 @@
 import React from 'react'
 //styles
 import s from "./check.module.scss"
+import { isDate } from 'util/types'
 
 const CheckAvailability = () => {
   return (
     <>
-        <div className={s.available}>
-            <form action="">
-                <div></div>
-                <label>Arrival date</label>
-                <input type="text" />
-            </form>
-        </div>
+      <div className={s.available}>
+        <form action="" className={s.form}>
+          <div></div>
+
+          <div className={s.form__block}>
+            <label className={s.form__block_label}>Arrival date</label>
+            <input className={s.form__block_input} type="date" />
+          </div>
+          <div className={s.form__block}>
+            <label className={s.form__block_label}>Departuure Date</label>
+            <input className={s.form__block_input} type="date" />
+          </div>
+          <div className={s.form__block}>
+            <label className={s.form__block_label}>Adults</label>
+            <input className={s.form__block_input} type="text" />
+          </div>
+          <div className={s.form__block}>
+            <label className={s.form__block_label}>Children</label>
+            <input className={s.form__block_input} type="text" />
+          </div>
+          <button className={s.form__button} type="submit" onClick={(e) => {
+                    e.preventDefault();
+                    alert("Checked")
+                }}>CHECK AVAILABILITY</button>
+        </form>
+      </div>
     </>
   )
 }

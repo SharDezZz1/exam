2e2a7037f60a2d6f63c266372327f744d7afc463
SharDezZz1 <andriypidkova27@gmail.com>
2023-06-09 23:45:19 +0300

    fix

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -7,20 +7,16 @@ import vec2 from "../../assets/MainPage/hero/Vector-1.png"
 import s from "./hero.module.scss"
 //components
 import CheckAvailability from '../CheckAvailability'
-import { url } from 'inspector'
 
-
-
-
-const Hero = (props: any) => {
+const Hero = () => {
     return (
         <>
             <section className={s.hero}>
                 <div></div>
                 <div className={s.hero__title}>
-                    <Image alt={""} src={vec1} ></Image>
-                    <h1 className={s.hero__title_text}>European romance comes to Nob Hill</h1>
-                    <Image alt={""} src={vec2} ></Image>
+                    <Image alt={""} src={vec1}></Image>
+                    <h1 className={s.hero__title_text}>European romance comes<br></br> to Nob Hill</h1>
+                    <Image alt={""} src={vec2}></Image>
                 </div>
                 <CheckAvailability></CheckAvailability>
             </section>
